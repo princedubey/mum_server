25dda112a997a3c4c453a7ca78706388b791519f
chiggosVikash <chiggos.vikash@gmail.com>
2024-11-17 16:22:48 +0530

    Added Favourite Schema

---
+++ src/models/Favourite.ts
@@ -0,0 +1,18 @@
+import mongoose, { model, Schema } from "mongoose";
+
+export interface Favourite {
+  userId: Schema.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+  likedBy: string; // me | heOrShe
+}
+
+const favouriteSchema = new Schema<Favourite>({
+  userId: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+  likedBy: { type: String, required: true },
+},{timestamps:true, versionKey: false});
+
+
+const FavouriteModel = mongoose.models?.Favourite || model<Favourite>("Favourite", favouriteSchema);
+
+export default FavouriteModel;
